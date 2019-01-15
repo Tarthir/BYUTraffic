14b4c0715813c3edcba003f2d7bc2565793942a3
Tarthir <tylerbrady34@gmail.com>
2019-01-15 11:44:46 -0700

    Added TODO

--- Traffic_Coordinator.sh
+++ Traffic_Coordinator.sh
@@ -39,6 +39,7 @@ python $(WD)/model/parser.py ${paths[3]}
 # Run p0fer.py, give path to .pcap files, and where the unzipped and final log files go
 python $(WD)${paths[0]} ${paths[2]} $(WD)/data/ $(WD)/data/final/
 # Run getASN.py, give path to where we will put asnData.data and give where final p0f log files are
+# TODO still need to get the non .pcap data run through getASN.py, will take some refactoring
 python $(WD)${paths[1]} $(WD)/data/asnData.data $(WD)/data/final/
 # Get all the asn data
 netcat whois.cymru.com 43 < $(WD)/data/asnData.asn | sort -n > $(WD)/data/final/asnFinalData.asn
