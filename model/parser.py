3039322ef80a91571af1ec7e0cefe49b68ea00f7
Kwankyu Ko <kwankyu.kko@gmail.com>
2019-01-17 15:52:18 -0700

    BYU Traffic parsing and log object created

--- model/parser.py
+++ model/parser.py
@@ -4,62 +4,53 @@ import glob
 import errno
 import gzip
 import re
+import LogList
 
-
+# Takes in the file path directory to read all the zip files
+# For Example from base /home/imaal/dns-queries/byu
 if len(sys.argv) != 2:
-    print("Error, ereojsldkakwanimwatchingyoufjld;fjalf")
+    print("Error, Give the path of the directory")
     sys.exit()
 
 
 class query_log:
-    def __init__(self):
-        self.date = None
-        self.time_zone = None
-        self.authoritative_name = None
-        self.process_name = None
-        self.process_id = None
-        self.client_ip = None
-        self.client_port = None
-        self.query = None
-        self.dns_class = None
-        self.resource_record = None
-        self.set = None
-        self.flag = None
-        self.authoritative_ip = None
+    def __init__(self, date, time, time_zone, authoritative_name, process_name, process_id,\
+    client_ip, client_port, query, dns_class, resource_record, set_bool, flag, authoritative_ip):
+        self.date = date
+        self.time = time
+        self.time_zone = time_zone
+        self.authoritative_name = authoritative_name
+        self.process_name = process_name
+        self.process_id = process_id
+        self.client_ip = client_ip
+        self.client_port = client_port
+        self.query = query
+        self.dns_class = dns_class
+        self.resource_record = resource_record
+        self.set = set_bool
+        self.flag = flag
+        self.authoritative_ip = authoritative_ip
         self.asn_data = None
         self.p0f_data = None
+    
+    def display(self):
+        print(str(self.date) + " " + str(self.time) + " " + str(self.time_zone) + " " + str(self.authoritative_name)\
+        + " " + str(self.process_name) + " " + str(self.process_id) + " " + str(self.client_ip) + " " + str(self.client_port)\
+        + " " + str(self.query) + " " + str(self.dns_class) + " " + str(self.resource_record) + " " + str(self.set)\
+        + " " + str(self.flag) + " " + str(self.authoritative_ip) + " " + str(self.asn_data) + " " + str(self.p0f_data))
 
     
-directory_name = sys.argv[1]
+directory_name = sys.argv[1] # Path of the directory holding the zip files
 
-# file_write = open(file_name, "r")
-
-print(directory_name)
-
-# for filename in glob.glob(os.path.join(directory_name, '*.gz')):
-#     # print(filename)
-#     try:
-#         with gzip.open(filename) as f:
-#             print(f)
-#             # sys.stdout.write(f.read())
-#     except IOError as exc:
-#         if exc.errno != errno.EISDIR:
-#             raise
+mighty_log = LogList()
 
 
-# for line in file_write:
-#     sys.stdout.write(line)
-    #print(line) #prints extra new line with output :(
-# The beautiful regex, not complete yet
-# (?i)^([0-9-]+)T([0-9:]+)-([0-9:]+) ([a-z0-9]+) ([a-z0-9]+)\[([0-9]+)\]: client ([0-9]{3}\.[0-9]{3}\.[0-9]{3}\.[0-9]{3})#([0-9]+) \(([a-z0-9.]+)\): query: ([a-z0-9.]+) ([a-z]+) ([a-z]+) -([a-z]+) \(([0-9]{3}\.[0-9]{3}\.[0-9]{3}\.[0-9]{3})\)
-
-
-
-output_file = open("sample_output.txt", "w")
+output_file = open("sample_output.txt", "w") # Creates a file with extra info about the files
 
 regex = re.compile(r'(?i)^([0-9-]+)T([0-9:]+)-([0-9:]+) ([a-z0-9]+) ([a-z0-9]+)\[([0-9]+)\]: client ([0-9a-f.:]+)#([0-9]+) \((.*?)\): query: (.*?) ([a-z]+) ([a-z0-9]+) ([-+])([a-z]+)* \((.*?)\)')
 
-for root, dirs, files in os.walk(sys.argv[1], topdown=True):
+total_line_count = 0
+for root, dirs, files in os.walk(sys.argv[1], topdown=True): # Start from the top and go down the directory
     for name in files:
         if name.endswith('.gz'):
             output_file.write(str(name) + ' ')
@@ -68,10 +59,19 @@ for root, dirs, files in os.walk(sys.argv[1], topdown=True):
                 for line in f:
                     line_count += 1
                     if re.match(regex, line):
-                        sys.stdout.write(line) # write to stdout prevents extra newline
+                        # sys.stdout.write(line) # write to stdout prevents extra newline
+                        regex_groups = re.match(regex, line)
+                        log_object = query_log(regex_groups.group(1), regex_groups.group(2), regex_groups.group(3), regex_groups.group(4), regex_groups.group(5)\
+                        , regex_groups.group(6), regex_groups.group(7), regex_groups.group(8), regex_groups.group(10), regex_groups.group(11)\
+                        , regex_groups.group(12), regex_groups.group(13), regex_groups.group(14), regex_groups.group(15))
+                        log_object.display()
+                        mighty_log.add_to_byu(log_object)
                     else:
                         sys.stderr.write(line)
             output_file.write(str(line_count) + '\n')
+            total_line_count += line_count
+
+output_file.write("Total Lines Read:" + str(total_line_count))
 #    for name in files:
 #       print(os.path.join(root, name))
 #    for name in dirs:
