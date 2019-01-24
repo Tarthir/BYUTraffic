dceee8d09161787c22876bb7d7467a8a935bafd8
Kwankyu Ko <kwankyu.kko@gmail.com>
2019-01-24 09:14:10 -0700

    dilt and byu traffic comparison completed

--- model/ditl_byu_comparison.py
+++ model/ditl_byu_comparison.py
@@ -8,6 +8,7 @@ if len(sys.argv) != 4:
     print("Path to the DITL file required")
     sys.exit()
 
+# python ditl_byu_comparison.py ~/Documents/RA-IMAAL/imaal-data/ditl-2018/resolver-count-all.txt.gz ~/Documents/RA-IMAAL/imaal-data/dns-queries/byu/testing-results2 ~/Documents/RA-IMAAL/imaal-data/dns-queries/byu/testing-results/
 
 # Store all ditl ips
 # Compare with BYU ips
@@ -22,6 +23,16 @@ if len(sys.argv) != 4:
 # Save the byu unique
 
 ditl_file = gzip.open(sys.argv[1], "r")
+byu_file = open(sys.argv[2], "r")
+
+ditl_list = []
+byu_list = []
+
+ditl_set = set()
+byu_set = set()
+
+test1= open(str(sys.argv[3]) + "ditl_sample", "w")
+test2= open(str(sys.argv[3]) + "byu_sample", "w")
 
 count = 0 #Debugging purpose
 for line in ditl_file:
@@ -29,14 +40,53 @@ for line in ditl_file:
     line_array = line.split()           #Breaks up the line by whitespace into an array
     ip = line_array[0]                  #Grabs the first word of the line/array (Which in this case is my IP address)
     ip = ip.decode("utf-8")             #At this point the ip is in byte form, so I decode in into utf-8 (string) Otherwise my output has b' signifying byte
-    ip_list.append(ip)                  #For now. Storing the IP into a list
-    out_file.write(ip)                  #Writes the lines to an output text file, it has to be in bytes yet
-    out_file.write('\n')                #Otherwise it is all one big mega IP address
-    if ":" in ip:
-        out_file_ipv6.write(ip)         #If it has : then it is ipv61
-        out_file_ipv6.write('\n')   
-    else:
-        # print(count)
-        count += 1
-        out_file_ipv4.write(ip)
-        out_file_ipv4.write('\n')
+    # ditl_list.append(ip)              # Using sets instead of lists
+    ditl_set.add(ip)
+    test1.write(str(ip) + "\n")
+
+for line in byu_file:
+    if line != "begin\n" and line != "end\n": # if reading from the ip's created from parser.py we don't want the begin and end needed for asn
+        # ip = line.decode("utf-8")
+        print(line)
+        ip = line.rstrip('\n')         # Get rid of the newline that comes with reading the file
+        # byu_list.append(ip)
+        byu_set.add(ip)
+        test2.write(str(ip))
+
+# Because I need to do something different for lists
+# def interSection(arr1,arr2): 
+  
+#      # filter(lambda x: x in arr1, arr2)  --> 
+#      # filter element x from list arr2 where x 
+#      # also lies in arr1 
+#      result = list(filter(lambda x: x in arr1, arr2))  
+#      print ("Intersection : ",result) 
+# intersection_list = ditl_list.intersection(byu_list)
+# ditl_unique = ditl_list - byu_list
+# byu_unique = byu_list - ditl_list
+
+
+# The ways of the sets
+intersection_set = ditl_set.intersection(byu_set)
+ditl_unique_set = ditl_set.difference(byu_set)
+byu_unique_set = byu_set.difference(ditl_set)
+
+test3 = open(str(sys.argv[3]) + "intersection", "w")
+test4 = open(str(sys.argv[3]) + "ditl_unique", "w")
+test5 = open(str(sys.argv[3]) + "byu_unique", "w")
+
+print("Intersection length: " + str(len(intersection_set)))
+print("DITL UNIQUE length: " + str(len(ditl_unique_set)))
+print("BYU UNIQUE length: " + str(len(byu_unique_set)))
+
+for a in intersection_set:
+    test3.write(a)
+    test3.write("\n")
+
+for b in ditl_unique_set:
+    test4.write(b)
+    test4.write("\n") 
+
+for c in byu_unique_set:
+    test5.write(c)
+    test5.write("\n")
--- model/parser.py
+++ model/parser.py
@@ -4,14 +4,17 @@ import glob
 import errno
 import gzip
 import re
+# import model.LogList as L
 import model.LogList as L
 import model.QueryLog as q
+# import QueryLog
+
 
 # Takes in the file path directory to read all the zip files
 # For Example from base /home/imaal/dns-queries/byu
 # Argument 1: Directory path to the zip giles
 # Argument 2: The file directory to which we want to save the binary data which happens in loglist save function
-# Argument 3: File name of client ips ready to be fed to asn reader
+# Argument 3: File name of client ips which will be fed to asn reader
 if len(sys.argv) != 4:
     print("Error, Give the path of the directory")
     sys.exit()
@@ -21,6 +24,7 @@ if len(sys.argv) != 4:
 directory_name = sys.argv[1] # Path of the directory holding the zip files
 
 mighty_log = L.LogList() # Our LogList object for which we store our data into
+# mighty_log = LogList() # Our LogList object for which we store our data into
 
 byu_to_asn_file = open(sys.argv[3], "w") # The file for which we store the client ip for asn. (begin....ip.....end)
 byu_to_asn_file.write("begin\n") # Because Tyler's asn file reader is so picky it nees this begin
@@ -41,6 +45,7 @@ for root, dirs, files in os.walk(sys.argv[1], topdown=True): # Start from the to
                     if re.match(regex, line):
                         # sys.stdout.write(line) # write to stdout prevents extra newline
                         regex_groups = re.match(regex, line) # Hold all the matched groups
+                        # log_object = q.query_log
                         log_object = q.query_log(regex_groups.group(1), regex_groups.group(2), regex_groups.group(3), regex_groups.group(4), regex_groups.group(5)\
                         , regex_groups.group(6), regex_groups.group(7), regex_groups.group(8), regex_groups.group(10), regex_groups.group(11)\
                         , regex_groups.group(12), regex_groups.group(13), regex_groups.group(14), regex_groups.group(15))
@@ -54,7 +59,7 @@ for root, dirs, files in os.walk(sys.argv[1], topdown=True): # Start from the to
             total_line_count += line_count # for sample output file
 
 byu_to_asn_file.write("end\n") # Write out to ASN
-mighty_log.save_data(sys.argv[2])
+# mighty_log.save_data(sys.argv[2])
 output_file.write("Total Lines Read:" + str(total_line_count) + "\n") # for sample output file
 #    for name in files:
 #       print(os.path.join(root, name))
