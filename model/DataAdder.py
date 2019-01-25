37d3884865a617d340c6716be23aef2109202a9d
Tarthir <tylerbrady34@gmail.com>
2019-01-25 08:55:09 -0700

    bug fixes

--- model/DataAdder.py
+++ model/DataAdder.py
@@ -1,13 +1,13 @@
 import sys
-import readers.P0fReader
-import readers.AsnReader
+import P0fReader
+import AsnReader
 import LogList
 
 log_list = LogList.LogList()
 # TODO we need the real file here
-log_list.load_all("/home/example/file/path/for/now/")
-p0fReader = readers.P0fReader.P0fReader()
-asnReader = readers.AsnReader.AsnReader()
+log_list.load_all(sys.argv[4])
+p0fReader = P0fReader.P0fReader()
+asnReader = AsnReader.AsnReader()
 
 # read the root asn data into the root data set
 asnReader.read(sys.argv[1], log_list)
@@ -16,4 +16,4 @@ asnReader.read(sys.argv[2], log_list)
 # read the p0f data into the byu data set
 p0fReader.read(sys.argv[3], log_list)
 # TODO this needs to become the real file
-log_list.save_data("/home/example/file/path/for/now/")
+log_list.save_data(sys.argv[4])
