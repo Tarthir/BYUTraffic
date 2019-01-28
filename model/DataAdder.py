aaaa50cafb2f2f6e6b36886aa35edd20f9ded028
Tarthir <tylerbrady34@gmail.com>
2019-01-28 13:57:13 -0700

    small bug with reading correct file

--- model/DataAdder.py
+++ model/DataAdder.py
@@ -15,5 +15,5 @@ asnReader.read(sys.argv[1], log_list)
 asnReader.read(sys.argv[2], log_list)
 # read the p0f data into the byu data set
 for file in os.listdir(sys.argv[3]):
-    p0fReader.read(file, log_list)
+    p0fReader.read(sys.argv[3] + file, log_list)
 log_list.save_data(sys.argv[4])
