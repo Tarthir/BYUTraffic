0e4713a26329a22c9fe337e9c2c0efc31f25ad0a
Kwankyu Ko <kwankyu.kko@gmail.com>
2019-02-06 15:48:13 -0700

    .

--- Traffic_Coordinator.sh
+++ Traffic_Coordinator.sh
@@ -19,20 +19,30 @@ mkdir $WD/data/p0f_files/
 
 
 # Run our parsing, give it the location of the queries.log files
+echo "PARSER.PY"
 python $WD/model/parser.py $loc2 $WD/data/ asnDataByu.asn
 # Run p0fer.py, give path to .pcap files, where the unzipped files go, and final log files go
+echo "P0FER.PY"
 python $WD/model/p0fer.py $loc $WD/data/unzipped_pcap/ $WD/data/p0f_files/
 # Run getASN.py, give path to where we will put asnDataByu.asn and give where final p0f log files are
+echo "GETASN.PY"
 python $WD/model/getASN.py $WD/data/asnDataRoot.asn $WD/data/p0f_files/
 # Get all the Byu asn data
+echo "BYU-ASN"
 netcat whois.cymru.com 43 < $WD/data/asnDataByu.asn | sort -n > $WD/data/asnFinalDataByu.asn
 # Get all the Root asn data
+echo "ROOT-ASN"
 netcat whois.cymru.com 43 < $WD/data/asnDataRoot.asn | sort -n > $WD/data/asnFinalDataRoot.asn
 # Venn diagram the ditl-2018 and byu authoritative traffic data
+echo "DITL_BYU_COMPARISON.PY"
 python $WD/model/ditl_byu_comparison.py $loc3 $WD/data/asnDataByu.asn $WD/data
 # Dig results - put them in a file
+echo "TTL_DIG.PY"
 python $WD/model/ttl_dig.py $WD/data/query_names_to_dig $WD/data/
 # Add all data we got from asn and p0f to our data sets
+echo "DATAADDER.PY"
 python3 $WD/model/DataAdder.py $WD/data/asnFinalDataRoot.asn $WD/data/asnFinalDataByu.asn $WD/data/p0f_files/ $WD/data/
+
+echo "DONE!"
 
 
